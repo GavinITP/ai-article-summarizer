d7237483ce6bd1e8921bae5fa5854d6c2b92f61d
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-08-04 20:09:55 +0700

    structure app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="font-inter">
+      <div className="w-full min-h-screen fixed pointer-events-none">
+        <div className="bg-black absolute w-full h-full" />
+      </div>
+
+      <div className="text-white relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;
